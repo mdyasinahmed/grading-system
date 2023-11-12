ba7700114633f12a3ac2bbdd3064d79ac73c67bb
mdyasinahmed <yasinahmed7875@gmail.com>
2023-11-12 09:30:12 +0600

    add grading system project root folder

---
+++ src/main/java/com/mycompany/grading/system/GradingSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.grading.system;
+
+/**
+ *
+ * @author YASIN
+ */
+public class GradingSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
